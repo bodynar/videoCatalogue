fffb11b11fdd9cfb894c9bef417a5cf34d464afd
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-05-01 20:42:45 +0300

    add pug

---
+++ src/app/video/video.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-video',
+    templateUrl: 'video.template.pug',
+    styles: ['h2 { color: green; }']
+})
+export class VideoComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
