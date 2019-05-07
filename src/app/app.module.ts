9a3ad4093f7a56431a970ea1f739dacd1b63d603
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-05-07 16:29:05 +0300

    move to folder root component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppContainerComponent } from './component/appContainer.component';
 
 import { MenuComponent } from './components/menu/menu.component';
 
@@ -15,7 +15,7 @@ import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppContainerComponent,
     MenuComponent
   ],
   imports: [
---
+++ src/app/component/appContainer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'appContainer.template.pug',
+  styleUrls: ['appContainer.style.styl']
+})
+class AppContainerComponent { }
+
+export { AppContainerComponent };
