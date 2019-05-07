b9aaf90fab219b742a71aedfeb1e03ece0eb3ccc
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-05-07 16:28:36 +0300

    add 404 page

---
+++ src/app/pages/notFound/notFound.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: 'notFound.template.pug',
+    styleUrls: ['notFound.style.styl']
+})
+class PageNotFoundComponent {
+    constructor() { }
+}
+
+export { PageNotFoundComponent };
