6da3e5a160db0abda5a895d0502494368fe3be1a
 Atom Malyshev <a.malyshev@itexpert.ru>
2019-05-11 15:13:52 +0300

    use welcome component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { areasRouting } from './areas/areas.routing';
+
+import { WelcomeComponent } from 'components/welcome/welcome.component';
 import { AppContainerComponent } from './components/appContainer/appContainer.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AppContainerComponent,
-    children: [...areasRouting]
+    children: [
+      ...areasRouting,
+      {
+        path: '',
+        component: WelcomeComponent
+      }
+    ]
   }
 ];
 
